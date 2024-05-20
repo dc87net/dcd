1355e8a4d10bd6f9c13402a679981f02e7535d64
douglas c <douglas@dc87.net>
2024-05-20 11:42:03 -0500

    reorganize/consolidate

--- cfunload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo killall -9 checkd;
-sudo launchctl unload system /Library/LaunchDaemons/com.cloudflare.1dot1dot1dot1.macos.warp.daemon.plist
-
--- hello.cpp
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-
-cout << "Hello, World!" << endl;
-
