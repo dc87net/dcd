cae0b3fbe044be46b02410ceb5590b290f38db36
douglas <douglas@dc87.net>
2024-11-17 08:42:08 -0600

    fix git

--- etc/junk/f.sh
+++
@@ -1,6 +0,0 @@
-log(){ echo "$*"; } #logger -t TLDW "$*"; }
- [[ $(pgrep 'Cloudflare WARP') ]] && {
-    log 'Loading WARP' &&{
-      ("/Applications/Cloudflare\ WARP.app/Contents/MacOS/Cloudflare\ WARP" || log 'Error spawning WARP');
-    };
-  }
